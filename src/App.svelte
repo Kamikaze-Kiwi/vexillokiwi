f91722a50a870b990349915fdbce8f0217bff118
MaikelHendrikx1 <84376526+MaikelHendrikx1@users.noreply.github.com>
2024-04-03 00:08:55 +0200

    Replace pngs with svg and implemented random flag and guessing logic

--- src/App.svelte
+++ src/App.svelte
@@ -1,47 +1,55 @@
 <script lang="ts">
-  import svelteLogo from './assets/svelte.svg'
-  import viteLogo from '/vite.svg'
-  import Counter from './lib/Counter.svelte'
+  import countries from './countries.json';
+
+  const maxStoredGuesses = 5;
+
+  let currentCountry = countries[0];
+  let currentGuess = '';
+
+  NextCountry();
+
+  // Make a guess for the current flag
+  function MakeGuess(e: Event) {
+    e.preventDefault();
+
+    if (currentGuess.toLowerCase() === currentCountry.name.toLowerCase()) {
+      alert('Correct!');
+      SaveGuess(true);
+    } else {
+      alert('Incorrect!');
+      SaveGuess(false);
+    }
+
+    NextCountry();
+  }
+
+  // Pick a random country and reset the guess
+  function NextCountry() {
+    let randomIndex = Math.floor(Math.random() * (countries.length - 1) + 1);
+    currentCountry = countries[randomIndex];
+    currentGuess = '';
+  };
+
+  function SaveGuess(correct: boolean) {
+    currentCountry.previousGuesses.push(correct);
+    if (currentCountry.previousGuesses.length > maxStoredGuesses) {
+      currentCountry.previousGuesses.shift();
+    }
+
+    localStorage.setItem('guess', currentGuess);
+  }
 </script>
 
 <main>
-  <div>
-    <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-      <img src={viteLogo} class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
-  <h1>Vite + Svelte</h1>
-
-  <div class="card">
-    <Counter />
-  </div>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
+  <img src={`Flags/${currentCountry.code.toLowerCase()}.svg`} alt="flag" height="400"/>
+  <br/>
+  <br/>
+  <form on:submit={MakeGuess}>
+    <input type="text" bind:value={currentGuess} placeholder="Your guess..." />
+    <button>Make guess</button>
+  </form>
 </main>
 
 <style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
-  }
+
 </style>
